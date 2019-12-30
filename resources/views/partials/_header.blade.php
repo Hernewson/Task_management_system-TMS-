25459575dffcd462c0f12dae9b53500dc6c6f649
Hernewson <hernewson8@gmail.com>
2019-12-30 12:17:38 +0545

    small changes

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -137,7 +137,7 @@
                     <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
                         data-close-others="true">
                         <img alt="" class="img-circle " src=" {{ asset('assets/img/dp.jpg') }} " />
-                        <span class="username username-hide-on-mobile"> User </span>
+                        <span class="username username-hide-on-mobile"> <b>{{Auth::user()->name}}</b> </span>
                         <i class="fa fa-angle-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-default">
